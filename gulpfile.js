f325841a9c0ee7af0a44db48b083b4e27f42543e
Vito Vattiata <vito.vattiata@olivetti.com>
2022-02-21 17:39:15 +0100

    localhost setup

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,7 @@ const header = require("gulp-header");
 const merge = require("merge-stream");
 const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
-const sass = require("gulp-sass");
-const uglify = require("gulp-uglify");
-
+const sass = require('gulp-sass')(require('sass'));const uglify = require("gulp-uglify");
 // Load package.json for banner
 const pkg = require('./package.json');
 
